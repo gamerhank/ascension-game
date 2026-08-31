b4c90cfb5a3ffe1da2818c8efad98b7d518ce472
hwschieding <hwschieding@gmail.com>
2026-08-31 19:04:26 -0400

    Flip weapon sprite based on x transform

--- scripts/weapons/BasicGun.cs
+++ scripts/weapons/BasicGun.cs
@@ -1,14 +1,18 @@
 using Godot;
 using System;
+using System.Security.Cryptography;
 
 public partial class BasicGun : Node2D
 {
 	[Export]
 	private PackedScene bulletScene;
 
+	private Sprite2D sprite;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		sprite = GetNode<Sprite2D>("Sprite2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,16 +23,24 @@ public partial class BasicGun : Node2D
 
 		if(Input.IsActionJustPressed("Shoot"))
 		{
-			// GD.Print("Shoot pressed, rotation=", Rotation);
-			Vector2 bulletSpawnPos = GlobalPosition;
-			float bulletSpawnDir = GlobalRotation;
-			Node2D bulletNode = bulletScene.Instantiate<Node2D>();
-			// bulletNode.Position = bulletSpawnPos;
-			// GD.Print("bullet rotation=", bulletNode.Rotation);
-			GetTree().CurrentScene.AddChild(bulletNode);
-
-			bulletNode.GlobalPosition = bulletSpawnPos;
-			bulletNode.Rotation = bulletSpawnDir;
+			spawnBullet();
 		}
+
+		// GD.Print(Transform.X.X);
+		Scale = Scale with { Y = Transform.X.X < 0.0f ? -1 : 1 };
+	}
+
+	private void spawnBullet()
+	{
+		// GD.Print("Shoot pressed, rotation=", Rotation);
+		Vector2 bulletSpawnPos = GlobalPosition;
+		float bulletSpawnDir = GlobalRotation;
+		Node2D bulletNode = bulletScene.Instantiate<Node2D>();
+		// bulletNode.Position = bulletSpawnPos;
+		// GD.Print("bullet rotation=", bulletNode.Rotation);
+		GetTree().CurrentScene.AddChild(bulletNode);
+
+		bulletNode.GlobalPosition = bulletSpawnPos;
+		bulletNode.Rotation = bulletSpawnDir;
 	}
 }
